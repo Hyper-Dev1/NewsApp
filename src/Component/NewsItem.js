9e149a884baf1c88bd69d34e3a260b6f99a20ffd
Hyper-Dev1 <sudipzdaahaal@gmail.com>
2023-02-17 20:37:31 +0545

    Ep-24 D17

---
+++ src/Component/NewsItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is News item2
+      </div>
+
+    )
+  }
+}
+
+export default NewsItem
