e78181846b15ca5cbb9c835df27ab15b7fbc7810
Hyper-Dev1 <sudipzdaahaal@gmail.com>
2023-02-27 18:22:20 +0545

    mern #33

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import './App.css';
+import "./App.css";
 
-
-import React, { Component } from 'react'
-import NavBar from './Component/NavBar'
-import News from './Component/News';
-
+import React, { Component } from "react";
+import NavBar from "./Component/NavBar";
+import News from "./Component/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
-  
+  pageSize = 5;
   render() {
     return (
       <div>
-        <NavBar/>
-        <News pageSize={5}/>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route Exact
+              path="/"
+              element={<News key= "general" pageSize={this.pageSize} country="us" category="general" />}
+            ></Route>
+            <Route Exact
+              path="/business"
+              element={<News key= "business" pageSize={this.pageSize} country="us" category="business" />}
+            ></Route>
+            <Route Exact
+              path="/Entertainment"
+              element={
+                <News key="entertainment" pageSize={this.pageSize} country="us" category="entertainment" />
+              }
+            ></Route>
+            <Route Exact
+              path="/Health"
+              element={<News key="health" pageSize={this.pageSize} country="us" category="health" />}
+            ></Route>
+            <Route Exact
+              path="/Science"
+              element={<News key="science" pageSize={this.pageSize} country="us" category="science" />}
+            ></Route>
+            <Route Exact
+              path="/Sports"
+              element={<News key="sports" pageSize={this.pageSize} country="us" category="sports" />}
+            ></Route>
+            <Route Exact
+              path="/Technology"
+              element={
+                <News key="technology" pageSize={30} country="us" category="technology" />
+              }
+            ></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
-    )
+    );
   }
 }
--- src/Component/News.js
+++ src/Component/News.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./spinner";
+import PropTypes from "prop-types";
 
 export class News extends Component {
+  static defaultProps = {
+    country: "us",
+    pageSize: 8,
+    category: "general",
+  };
+  static propsTypes = {
+    country: PropTypes.string,
+    category: PropTypes.string,
+    pageSize: PropTypes.number,
+  };
+
   constructor() {
     super();
     // console.log("hello constructor");
@@ -11,69 +24,58 @@ export class News extends Component {
       page: 1,
     };
   }
-
-  async componentDidMount() {
-    let url =
-      `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=f5474f19e8b5486ea82dc0a775ebe69a&pageSize=${this.props.pageSize}`;
+  async update() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f5474f19e8b5486ea82dc0a775ebe69a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({ articles: parsedData.articles });
     this.setState({
       articles: parsedData.articles,
       totalResults: [parsedData.totalResults],
+      loading: false,
     });
+  }
+  async componentDidMount() {
+    this.update();
   }
 
   handlePrevClick = async () => {
-    console.log("Previous");
-
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=f5474f19e8b5486ea82dc0a775ebe69a&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-    });
+    this.setState({ page: this.state.page - 1 });
+    this.update();
   };
+
   handleNextClick = async () => {
-    console.log("Next");
-
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) {
-    } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=f5474f19e8b5486ea82dc0a775ebe69a&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-      });
-    }
+    this.setState({ page: this.state.page + 1 });
+    this.update();
   };
+  
   render() {
     return (
       <div className="container my-3">
         <h1 className="text-center">NewsMonkey</h1>
-        <Spinner/>
+        {this.state.loading && <Spinner />}
         <div className="row my-3">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title}
-                  description={
-                    element.description ? element.description.slice(0, 90) : ""
-                  }
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 90)
+                        : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-evenly">
           <button
